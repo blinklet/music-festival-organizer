2ca887c7286d63b4644663f46353086aa779a9bd
Brian Linkletter <mail@brianlinkletter.ca>
2024-07-17 15:59:45 -0400

    Added profile edit to user registration

--- mfo/account/forms/edit_profile.py
+++ mfo/account/forms/edit_profile.py
@@ -1,15 +1,50 @@
 import wtforms
 import phonenumbers
+import datetime
 from flask_wtf import FlaskForm
 
 
+def validate_at_least_one_role(form, field):
+    if not field.data or len(field.data) == 0:
+        raise wtforms.ValidationError("At least one role must be selected")
+
 class ProfileEdit(FlaskForm):
+
+    def validate_birthday(form, field):
+        # I used a the name "validate_birthday" instead of "validate_birthdate"
+        # because the field is named "birthdate" in the form. This avoids a
+        # doubled validation error message.
+        
+        if not field.data:
+            # workaround for wtforms validation bug
+            # https://stackoverflow.com/questions/37026390/dont-want-to-validate-a-datefield-in-wtforms-but-want-to-keep-the-date-format
+            field.errors[:] = []
+
+            if 'Participant' in form.rolenames.data:
+                raise wtforms.ValidationError('Participants must enter their birthdate')
+        else:
+            if field.data > datetime.date.today():
+                raise wtforms.ValidationError('Birthdate is in the future.')
+            
+    def validate_phone(form, field):
+    # https://stackoverflow.com/questions/36251149/validating-us-phone-number-in-wtforms
+        if field.data:
+            if len(field.data) > 16:
+                raise wtforms.ValidationError('Invalid phone number.')
+            try:
+                input_number = phonenumbers.parse(field.data)
+                if not (phonenumbers.is_valid_number(input_number)):
+                    raise wtforms.ValidationError('Invalid phone number.')
+            except:
+                input_number = phonenumbers.parse("+1"+field.data)
+                if not (phonenumbers.is_valid_number(input_number)):
+                    raise wtforms.ValidationError('Invalid phone number.')
 
     name = wtforms.StringField(
         'Full Name', 
         [wtforms.validators.InputRequired()]
         )
-    birthdate = wtforms.DateField('Birth Date')
+    birthdate = wtforms.DateField('Birth Date', [validate_birthday])
 
     address = wtforms.StringField('Street Address')
     city = wtforms.StringField('City')
@@ -24,18 +59,21 @@ class ProfileEdit(FlaskForm):
     national_festival  =  wtforms.BooleanField(
         'Eligible for the National Festival'
         )
-
+    
+    rolenames = wtforms.SelectMultipleField(
+        'Roles',
+        choices=[
+            ('Participant', 'Participant'), 
+            ('Teacher', 'Teacher'), 
+            ('Guardian', 'Parent or Guardian'), 
+            ('Accompanist', 'Accompanist'), 
+            ('Adjudicator', 'Adjudicator')
+            ],
+        default=['Participant'],
+        option_widget=wtforms.widgets.CheckboxInput(),
+        widget=wtforms.widgets.ListWidget(prefix_label=False),
+        validators=[validate_at_least_one_role]
+    )
     submit = wtforms.SubmitField('Save changes')
 
-    # https://stackoverflow.com/questions/36251149/validating-us-phone-number-in-wtforms
-    def validate_phone(form, field):
-        if len(field.data) > 16:
-            raise wtforms.ValidationError('Invalid phone number.')
-        try:
-            input_number = phonenumbers.parse(field.data)
-            if not (phonenumbers.is_valid_number(input_number)):
-                raise wtforms.ValidationError('Invalid phone number.')
-        except:
-            input_number = phonenumbers.parse("+1"+field.data)
-            if not (phonenumbers.is_valid_number(input_number)):
-                raise wtforms.ValidationError('Invalid phone number.')
+    
--- mfo/account/views.py
+++ mfo/account/views.py
@@ -3,8 +3,11 @@ import flask
 from mfo.database.base import db
 import mfo.database.utilities
 import flask_security
+from sqlalchemy import select
 
 from mfo.account.forms.edit_profile import ProfileEdit
+from mfo.database.users import Role
+from mfo.database.models import Profile
 
 bp = flask.Blueprint(
     'account',
@@ -21,26 +24,63 @@ def index():
     profile = mfo.database.utilities.find_primary_profile(user)
     return flask.render_template('/account/index.html', user=user, profile=profile)
 
-@bp.route('/edit_profile', methods=['GET', 'POST'])
+@bp.post('/edit_profile')
 @flask_security.auth_required()
-def edit_profile():
+def edit_profile_post():
     user = flask_security.current_user
     profile = mfo.database.utilities.find_primary_profile(user)
+    profile.rolenames = [role.name for role in profile.roles]
     form = ProfileEdit(obj=profile)
 
     if form.validate_on_submit():
         profile.name = form.name.data
-        profile.birthdate = form.birthdate.data
-        profile.address = form.address.data
-        profile.city = form.city.data
-        profile.province = form.province.data
-        profile.postal_code = form.postal_code.data
-        profile.phone = form.phone.data
-        profile.school = form.school.data
-        profile.teacher = form.teacher.data
-        profile.comments = form.comments.data
-        profile.national_festival = form.national_festival.data
+        profile.birthdate = form.birthdate.data if form.birthdate.data else None
+        profile.address = form.address.data if form.address.data else None
+        profile.city = form.city.data if form.city.data else None
+        profile.province = form.province.data if form.province.data else None
+        profile.postal_code = form.postal_code.data if form.postal_code.data else None
+        profile.phone = form.phone.data if form.phone.data else None
+        profile.school = form.school.data if form.school.data else None
+        profile.teacher = form.teacher.data if form.teacher.data else None
+        profile.comments = form.comments.data if form.comments.data else None
+        profile.national_festival = form.national_festival.data if form.national_festival.data else None
+
+        # Get all role names except admin
+        stmt = select(Role).where(Role.name != 'Admin')
+        all_roles = db.session.execute(stmt).scalars().all()
+        all_role_names = {role.name for role in all_roles}
+
+        selected_role_names = set(form.rolenames.data)
+
+        # Remove unselected roles from profile
+        for role in all_roles:
+            if role.name not in selected_role_names:
+                if role in profile.roles:
+                    profile.roles.remove(role)
+                if role in user.roles: 
+                    user.roles.remove(role)
+                
+        for selected_role in selected_role_names:
+            stmt = select(Role).where(Role.name==selected_role)
+            role = db.session.execute(stmt).scalars().first()
+            if role not in profile.roles:
+                profile.roles.append(role)
+            # Also add roles to user because I do not have the
+            # automatic cascading set up
+            if role not in user.roles:
+                user.roles.append(role)
+
         db.session.commit()
         return flask.redirect(flask.url_for('account.index'))
+    
+    return flask.render_template('/account/edit_profile.html', form=form)
 
+
+@bp.get('/edit_profile')
+@flask_security.auth_required()
+def edit_profile_get():
+    user = flask_security.current_user
+    profile = mfo.database.utilities.find_primary_profile(user)
+    profile.rolenames = [role.name for role in profile.roles]
+    form = ProfileEdit(obj=profile)
     return flask.render_template('/account/edit_profile.html', form=form)
